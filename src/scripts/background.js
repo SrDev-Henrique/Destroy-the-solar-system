458fdc3570604744234ca81bff137f4325eb51ef
Henrique Albuquerque <halbuquerque2850@gmail.com>
2024-11-27 12:19:25 -0300

    Change the skybox images

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -2,12 +2,12 @@ import * as THREE from "three";
 
 const loader = new THREE.CubeTextureLoader();
 const cubeTexture = loader.load([
-  "../Skybox/posX.jpg", // positivo X
-  "../Skybox/negX.jpg", // negativo X
-  "../Skybox/posY.jpg", // positivo Y
-  "../Skybox/negY.jpg", // negativo Y
-  "../Skybox/posZ.jpg", // positivo Z
-  "../Skybox/negZ.jpg", // negativo Z
+  "../Skybox/posX.png", // positivo X
+  "../Skybox/negX.png", // negativo X
+  "../Skybox/posY.png", // positivo Y
+  "../Skybox/negY.png", // negativo Y
+  "../Skybox/posZ.png", // positivo Z
+  "../Skybox/negZ.png", // negativo Z
 ]);
 
 export default cubeTexture;
